98902bc649394bf6cd45446bfe2ea4a890155016
Ghulam Ali <132977700+ghulamali17@users.noreply.github.com>
2024-12-23 14:48:46 +0500

    make navbar responsive with improved mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { toggle, clearUser } from "../../Redux/slice.js";
 import { getAuth, signOut } from "firebase/auth";
 import Dropdown from "../Dropdown.jsx";
 import DarkMode from "../DarkMode/DarkMode.jsx";
+import { RiCloseLargeFill } from "react-icons/ri";
 
 function Navbar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const [menuOpen, setMenuOpen] = useState(false);
 
   const isToggled = useSelector((state) => state.myCart?.toggle || false);
   const user = useSelector((state) => state.myCart?.user);
@@ -37,10 +40,8 @@ function Navbar() {
   // Active class logic for Nav Links
   const getActiveClass = ({ isActive }) =>
     isActive
-      ? `text-center p-3 mt-6 hover:bg-gray-800 hover:text-white ${
-          isToggled ? "bg-black text-white" : "bg-white text-black"
-        }`
-      : `hover:bg-gray-800 text-center p-3 mt-6 text-gray-500 hover:text-white`;
+      ? "text-center p-3 mt-6 bg-black text-white hover:bg-gray-800"
+      : "hover:bg-gray-800 text-center p-3 mt-6 text-gray-500 hover:text-white";
 
   return (
     <nav
@@ -55,34 +56,126 @@ function Navbar() {
         />
         <h1 className="self-center text-4xl font-semibold">FASHION</h1>
       </div>
-      <button>
-        <i
-          className="fa-solid fa-bars md:hidden dark:bg-gray-800
-               md:dark:bg-gray-900 dark:border-gray-700 dark:text-white text-2xl"
-        ></i>
+      {/* Hamburger  */}
+      <button onClick={() => setMenuOpen(true)} className="tablet:hidden block">
+        <i className="fa-solid fa-bars dark:bg-gray-800 dark:border-gray-700 dark:text-white text-2xl"></i>
       </button>
+      {/* Sidebar mobile menu  */}
+      <div
+        onClick={() => setMenuOpen(false)}
+        className={`fixed h-full w-screen lg:hidden 
+  bg-black/50 backdrop-blur-sm top-0 right-0 
+  ${menuOpen ? "flex opacity-100" : "hidden opacity-0"} 
+  transition-opacity transform duration-700 ease-in-out`}
+      >
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="text-black bg-white w-56 flex-col absolute left-0 top-0 h-screen p-8 gap-8 z-50 flex"
+        >
+          <RiCloseLargeFill
+            onClick={() => setMenuOpen(false)}
+            className="mt-0 mb-8 text-3xl cursor-pointer"
+          />
+          <ul className="uppercase flex flex-col gap-10 text-gray-500 items-center">
+            <li>
+              <NavLink
+                to="/"
+                className={getActiveClass}
+                onClick={() => setMenuOpen(false)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/shop"
+                className={getActiveClass}
+                onClick={() => setMenuOpen(false)}
+              >
+                Shop
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink
+                  to="/cart"
+                  className={getActiveClass}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Cart
+                </NavLink>
+              </li>
+            )}
+            {!user ? (
+              <li>
+                <NavLink
+                  to="/signup"
+                  className={getActiveClass}
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Sign Up
+                </NavLink>
+              </li>
+            ) : (
+              <li>
+                <button
+                  onClick={() => {
+                    logOutHandler();
+                    setMenuOpen(false);
+                  }}
+                  className="hover:underline"
+                >
+                  Log Out
+                </button>
+              </li>
+            )}
+            <li>
+              <NavLink
+                to="/admin-login"
+                className={getActiveClass}
+                onClick={() => setMenuOpen(false)}
+              >
+                Admin
+              </NavLink>
+            </li>
+            <li
+              className="cursor-pointer text-xl flex items-center"
+              onClick={toggleThemeHandler}
+            >
+              <DarkMode />
+            </li>
+            {user && (
+              <li className="flex items-center gap-2 cursor-pointer mt-3 text-gray-500">
+                <Dropdown />
+                <span className="font-semibold text-xl">{displayName}</span>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
 
-      <ul className="uppercase hidden md:flex gap-10 text-gray-500 items-center">
+      {/* Navbar Desktop  */}
+      <ul className="uppercase hidden tablet:flex gap-10 text-gray-500 items-center">
         <li>
-          <NavLink to="/" className={getActiveClass}>
+          <NavLink to="/" className={getActiveClass(false)}>
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/shop" className={getActiveClass}>
+          <NavLink to="/shop" className={getActiveClass(false)}>
             Shop
           </NavLink>
         </li>
         {user && (
           <li>
-            <NavLink to="/cart" className={getActiveClass}>
+            <NavLink to="/cart" className={getActiveClass(false)}>
               Cart
             </NavLink>
           </li>
         )}
         {!user ? (
           <li>
-            <NavLink to="/signup" className={getActiveClass}>
+            <NavLink to="/signup" className={getActiveClass(false)}>
               Sign Up
             </NavLink>
           </li>
@@ -94,7 +187,7 @@ function Navbar() {
           </li>
         )}
         <li>
-          <NavLink to="/admin-login" className={getActiveClass}>
+          <NavLink to="/admin-login" className={getActiveClass(false)}>
             Admin
           </NavLink>
         </li>
@@ -106,7 +199,7 @@ function Navbar() {
         </li>
         {user && (
           <li className="flex items-center gap-2 cursor-pointer mt-3 text-gray-500">
-            <Dropdown/>
+            <Dropdown />
             <span className="font-semibold text-xl">{displayName}</span>
           </li>
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,20 @@ export default {
     extend: {
       screens: {
         xs: "320px",
+        tablet: "769px",
       },
       boxShadow: {
         "custom-shadow": "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       },
       fontFamily: {
-        poppins: ['"Poppins"', "sans-serif"], 
+        poppins: ['"Poppins"', "sans-serif"],
       },
       fontWeight: {
         medium: "500",
         regular: "400",
       },
       colors: {
-        primaryColor: '#E6C744',
+        primaryColor: "#E6C744",
       },
     },
   },
